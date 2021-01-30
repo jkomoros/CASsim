d72470aa51a93409e4ecc065dd490a403bddc6d8
Alex Komoroske <jkomoros@gmail.com>
2021-01-30 12:00:23 -0800

    Fix another lint issue

--- src/components/map-visualization.js
+++ src/components/map-visualization.js
@@ -8,7 +8,7 @@ Code distributed by Google as part of the polymer project is also
 subject to an additional IP rights grant found at http://polymer.github.io/PATENTS.txt
 */
 
-import { LitElement, html, css } from 'lit-element';
+import { LitElement, html } from 'lit-element';
 
 // This is a reusable element. It is not connected to the store. You can
 // imagine that it could just as well be a third-party element that you
