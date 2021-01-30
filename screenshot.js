c23f0410c80d112d861544844c06431832819a46
Alex Komoroske <jkomoros@gmail.com>
2021-01-30 14:27:10 -0800

    Create a screenshots directory to save to. Part of #4.

--- screenshot.js
+++ screenshot.js
@@ -1,11 +1,18 @@
 /*eslint-env node*/
 
 const puppeteer = require("puppeteer");
+const fs = require('fs');
+
+const SCREENSHOT_DIR = 'screenshots';
 
 (async() => {
+	if (!fs.existsSync(SCREENSHOT_DIR)) {
+		fs.mkdirSync(SCREENSHOT_DIR);
+	}
+
 	const browser = await puppeteer.launch({args: ['--no-sandbox', '--disable-setuid-sandbox']});
 	const page = await browser.newPage();
 	await page.goto('http://localhost:8081');
-	await page.screenshot({path: 'screenshot.png'});
+	await page.screenshot({path: SCREENSHOT_DIR + '/screenshot.png'});
 	await browser.close();
 })();
