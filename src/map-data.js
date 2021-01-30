2a3a98c06a69eb44dcce348f2287c972d8405800
Alex Komoroske <jkomoros@gmail.com>
2021-01-30 11:18:53 -0800

    Roughed in some of the logic for the set_size command.

--- src/map-data.js
+++ src/map-data.js
@@ -18,16 +18,45 @@ where cell data looks like:
 
 */
 
+//Expects an array of [rows, cols] for size of map.
+const SET_SIZE_COMMAND = 'set_size';
+
+const defaultCellData = (row, col) => {
+	return {
+		row,
+		col,
+		value: 0.0,
+		highlighted: false,
+		selected: false,
+		opacity: 1.0,
+	}
+}
+
+const defaultMapForSize = (rows, cols) => {
+	const result = [];
+	for (let r = 0; r < rows; r++) {
+		for (let c = 0; c < cols; c++) {
+			result.push(defaultCellData(row, col));
+		}
+	}
+	return result;
+}
 
 class visualizationMap {
-	constructor(collection, index, data) {
+	constructor(collection, index, rawData) {
 		this._collection = collection;
 		this._index = index;
-		this._data = data;
+		this._rawData = data;
 		this._cachedData = null;
 	}
 
 	_computeData() {
+		const size = this._rawData[SET_SIZE_COMMAND];
+		if (!size) throw new Error('First item did not have a set size');
+		if (!Array.isArray(size)) throw new Error('size command not an array');
+		if (size.length != 2) throw new Error('Size command expects array of lenght 2');
+		const result = defaultMapForSize(...size);
+
 		//TODO: compute the final data model here.
 		this._cachedData = {};
 	}
