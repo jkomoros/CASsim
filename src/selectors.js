c95432f67b1f9bb4b86a38792986adb3d70a74b2
Alex Komoroske <jkomoros@gmail.com>
2021-01-31 07:08:31 -0800

    Actually start out all the way at the end even with repeat blocks. Part of #16.

--- src/selectors.js
+++ src/selectors.js
@@ -7,22 +7,22 @@ import {
 const selectRawMapData = state => state.data ? state.data.data : [];
 export const selectRawCurrentDataIndex = state => state.data ? state.data.index : 0;
 
-export const selectCurrentDataIndex = createSelector(
-	selectRawCurrentDataIndex,
-	selectRawMapData,
-	(rawIndex, mapData) => {
-		if (rawIndex >= 0) return rawIndex;
-		if (mapData.length == 0) return 0;
-		return mapData.length - 1;
-	}
-);
-
 export const selectPage = state => state.app ? state.app.page : '';
 export const selectPageExtra = state => state.app ? state.app.pageExtra : '';
 
 const selectVisualizationMapCollection = createSelector(
 	selectRawMapData,
 	(rawMapData) => new VisualizationMapCollection(rawMapData)
+);
+
+export const selectCurrentDataIndex = createSelector(
+	selectRawCurrentDataIndex,
+	selectVisualizationMapCollection,
+	(rawIndex, collection) => {
+		if (rawIndex >= 0) return rawIndex;
+		if (collection.length == 0) return 0;
+		return collection.length - 1;
+	}
 );
 
 export const selectMaxLegalIndex = createSelector(
