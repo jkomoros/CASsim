19d51539d46ecb2debe12e62d26b9d255bfb4c72
Alex Komoroske <jkomoros@gmail.com>
2021-11-25 07:48:33 -0800

    Add a test. Part of #25.

--- test/options/test.js
+++ test/options/test.js
@@ -1,19 +1,36 @@
 /*eslint-env node*/
 
 import {
-	setPropertyInObject
+	setPropertyInObject,
+	deepFreeze
 } from '../../src/util.js';
 
 import assert from 'assert';
 
-describe('getObjectPath', () => {
+describe('setPropertyInObject', () => {
 	it('handles null object', async () => {
 		const obj = null;
-		const path = ['a.b'];
+		const path = 'a.b';
 		const value = '3';
 		const result = setPropertyInObject(obj, path, value);
 		const golden = undefined;
 		assert.strictEqual(result, golden);
+	});
+
+	it('handles a single-nexted object', async () => {
+		const obj = {
+			a: 1,
+			b: 2,
+		};
+		deepFreeze(obj);
+		const path = 'a';
+		const value = '3';
+		const result = setPropertyInObject(obj, path, value);
+		const golden = {
+			a: 3,
+			b: 2
+		};
+		assert.deepEqual(result, golden);
 	});
 	
 });
