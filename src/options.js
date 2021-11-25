82c985151f2ddeaaba4953c22aa4cb77a1e88edd
Alex Komoroske <jkomoros@gmail.com>
2021-11-25 13:54:37 -0800

    Remove a completed TODO comment. Part of #25.

--- src/options.js
+++ src/options.js
@@ -272,7 +272,6 @@ export const maySetPropertyInConfigObject = (optionsConfig, path, value) => {
 			}
 		}
 	}
-	//TODO: make sure sub-sets of objects missing sub-objects that are not nullable fails.
 	//TODO: deleting
 	//TODO: test very hard objects like the individuals array
 	return [];
