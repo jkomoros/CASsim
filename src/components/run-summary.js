95df688a1facfc57fca6edfbd1583094f8b436c5
Alex Komoroske <jkomoros@gmail.com>
2021-11-24 20:18:50 -0800

    Render success percentage of runs. Part of #10

--- src/components/run-summary.js
+++ src/components/run-summary.js
@@ -40,9 +40,13 @@ class RunSummary extends LitElement {
 	}
 
 	render() {
+		const successCount = this.statuses.map(value => value == 1.0 ? 1.0 : 0.0).reduce((prev, curr) => prev + curr);
+		const successPercentage = '' + Math.floor(100 * successCount / this.statuses.length) + '%';
+
 		return html`
 				<div class='statuses'>
 					${this.statuses.map((status, index) => html`<div class='status ${this.selectedIndex == index ? 'selected' : ''} ${status < 0 ? 'indeterminate' : (status == 1.0 ? 'success' : 'failure')}' @click=${this._handleStatusClicked} .index=${index}></div>`)}
+					<span>${successPercentage}</span>
 				</div>
 		`;
 	}
