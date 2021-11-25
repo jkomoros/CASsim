47d983b17e9e76348f5b4a38018150e160d3b3f4
Alex Komoroske <jkomoros@gmail.com>
2021-11-25 11:19:39 -0800

    Split into two functions. Part of #25.

--- src/options.js
+++ src/options.js
@@ -1,5 +1,14 @@
+const EXAMPLES_PROPERTY_NAME = 'examples';
+
 //Returns an array of strings describing the problems in it or [] if no problems.
 export const optionsConfigValidator = (config) => {
 	if (!config) return ['Config must be an object'];
+	//The top-level expectation is basically an object with examples.
+	return innerOptionsConfigValidator({
+		[EXAMPLES_PROPERTY_NAME]: config,
+	});
+};
+
+const innerOptionsConfigValidator = (config) => {
 	return [];
 };
