e398dcdfb6de361095d8424b98eb5a360acd9a09
Alex Komoroske <jkomoros@gmail.com>
2021-11-26 11:41:48 -0800

    Very basic controls rendering in a nested way. Part of #25.

--- src/components/options-control.js
+++ src/components/options-control.js
@@ -3,6 +3,7 @@ import { LitElement, html, css } from "lit-element";
 class OptionsControl extends LitElement {
 	static get properties() {
 		return {
+			name: {type:String},
 			config: {type: Object},
 			value: {type: Object},
 		};
@@ -10,14 +11,37 @@ class OptionsControl extends LitElement {
 
 	static get styles() {
 		return [
-			css``
+			css`
+				.container {
+					display: flex;
+					flex-direction: column;
+				}
+
+				options-control {
+					margin-left: 0.5em;
+				}
+			`
 		];
 	}
 
 	render() {
+		return html`<div class='container'>
+			${this._inner()}
+		</div>`;
+	}
+
+	_inner() {
+		const example = this.config.example;
+		if (this.config.example == undefined) {
+			return html`${Object.entries(this.config).map(entry => html`<options-control .value=${this.value[entry[0]]} .config=${entry[1]} .name=${entry[0]}></options-control>`)}`;
+		}
 		return html`
-            <pre>${JSON.stringify(this.config, '', 2)}</pre>
-            <pre>${JSON.stringify(this.value, '', 2)}</pre>
+			<label>${this.name} - ${this.config.description}</label>
+			${typeof example == 'object' ?
+		(Array.isArray(example) ?
+			html`${this.value.map((item, index) => html`<options-control .value=${item} .config=${this.config[0]} .name=${index}></options-control>`)}` :
+			html`${Object.entries(this.value).map(entry => html`<options-control .value=${entry[1]} .config=${example[entry[0]]} .name=${entry[0]}></options-control>`)}`) :
+		html`<input .value=${this.value}></input>`}
 		`;
 	}
 
