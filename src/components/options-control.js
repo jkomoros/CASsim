1a8f371436337b1cf4ce0b38c22ad22543de8de0
Alex Komoroske <jkomoros@gmail.com>
2021-11-26 13:28:13 -0800

    Have a path property passed down into each input. Part of #25.

--- src/components/options-control.js
+++ src/components/options-control.js
@@ -8,6 +8,7 @@ import {
 class OptionsControl extends LitElement {
 	static get properties() {
 		return {
+			path: {type:String},
 			name: {type:String},
 			config: {type: Object},
 			value: {type: Object},
@@ -37,18 +38,24 @@ class OptionsControl extends LitElement {
 		</div>`;
 	}
 
+	_dottedPath(nextPart) {
+		const path = this.path || '';
+		if (!path) return nextPart;
+		return path + '.' + nextPart;
+	}
+
 	_inner() {
 		const example = this.config.example;
 		if (this.config.example == undefined) {
-			return html`${Object.entries(this.config).map(entry => html`<options-control .value=${this.value[entry[0]]} .config=${entry[1]} .name=${entry[0]}></options-control>`)}`;
+			return html`${Object.entries(this.config).map(entry => html`<options-control .value=${this.value[entry[0]]} .config=${entry[1]} .name=${entry[0]} .path=${this._dottedPath(entry[0])}></options-control>`)}`;
 		}
 		return html`
 			<label>${this.name} ${help(this.config.description)}</label>
 			${typeof example == 'object' ?
 		(Array.isArray(example) ?
-			html`${this.value.map((item, index) => html`<options-control .value=${item} .config=${example[0]} .name=${index}></options-control>`)}` :
+			html`${this.value.map((item, index) => html`<options-control .value=${item} .config=${example[0]} .name=${index} .path=${this._dottedPath(index)}></options-control>`)}` :
 			(this.value == null ? html`<em>null</em>` :
-				html`${Object.entries(this.value).map(entry => html`<options-control .value=${entry[1]} .config=${example[entry[0]]} .name=${entry[0]}></options-control>`)}`)) :
+				html`${Object.entries(this.value).map(entry => html`<options-control .value=${entry[1]} .config=${example[entry[0]]} .name=${entry[0]} .path=${this._dottedPath(entry[0])}></options-control>`)}`)) :
 		html`<input .type=${typeof example == 'number' ? 'number' : (typeof example =='string' ? 'text' : 'checkbox')} .min=${this.config.min || 0.0} .max=${this.config.max || Number.MAX_SAFE_INTEGER} .step=${this.config.step || 1.0} .value=${this.value} .checked=${this.value}></input>`}
 		`;
 	}
