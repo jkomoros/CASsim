11116feb1117070b1c57e05c962eb34ca8cd59e2
Alex Komoroske <jkomoros@gmail.com>
2021-11-26 14:21:08 -0800

    Split out rendering of different controls to be easier to modify than nested in templates. Part of #25.

--- src/components/options-control.js
+++ src/components/options-control.js
@@ -46,18 +46,27 @@ class OptionsControl extends LitElement {
 
 	_inner() {
 		const example = this.config.example;
-		if (this.config.example == undefined) {
+		if (example == undefined) {
 			return html`${Object.entries(this.config).map(entry => html`<options-control .value=${this.value[entry[0]]} .config=${entry[1]} .name=${entry[0]} .path=${this._dottedPath(entry[0])}></options-control>`)}`;
 		}
 		return html`
 			<label>${this.name} ${help(this.config.description)}</label>
-			${typeof example == 'object' ?
-		(Array.isArray(example) ?
-			html`${this.value.map((item, index) => html`<options-control .value=${item} .config=${example[0]} .name=${index} .path=${this._dottedPath(index)}></options-control>`)}` :
-			(this.value == null ? html`<em>null</em>` :
-				html`${Object.entries(this.value).map(entry => html`<options-control .value=${entry[1]} .config=${example[entry[0]]} .name=${entry[0]} .path=${this._dottedPath(entry[0])}></options-control>`)}`)) :
-		html`<input @change=${this._handleInputChanged} .type=${typeof example == 'number' ? 'number' : (typeof example =='string' ? 'text' : 'checkbox')} .min=${this.config.min || 0.0} .max=${this.config.max || Number.MAX_SAFE_INTEGER} .step=${this.config.step || 1.0} .value=${this.value} .checked=${this.value}></input>`}
+			${this._innerControl()}
 		`;
+	}
+
+	_innerControl() {
+		const example = this.config.example;
+		if (typeof example == 'object') {
+			if (Array.isArray(example)) {
+				return html`${this.value.map((item, index) => html`<options-control .value=${item} .config=${example[0]} .name=${index} .path=${this._dottedPath(index)}></options-control>`)}`;
+			}
+			if (this.value == null) {
+				return html`<em>null</em>`;
+			}
+			return html`${Object.entries(this.value).map(entry => html`<options-control .value=${entry[1]} .config=${example[entry[0]]} .name=${entry[0]} .path=${this._dottedPath(entry[0])}></options-control>`)}`;
+		}
+		return html`<input @change=${this._handleInputChanged} .type=${typeof example == 'number' ? 'number' : (typeof example =='string' ? 'text' : 'checkbox')} .min=${this.config.min || 0.0} .max=${this.config.max || Number.MAX_SAFE_INTEGER} .step=${this.config.step || 1.0} .value=${this.value} .checked=${this.value}></input>`;
 	}
 
 	_handleInputChanged(e) {
