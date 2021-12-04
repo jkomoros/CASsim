jkomoros/CASsim

commit 64044fb798ef8aacf1e04f91566d432d24c64a70

Make the step validation logic more forgiving of rare but possible mismatches (e.g 0.15 with a step of 0.05 before returned false). Part of #25.

src/util.js
@@ -44,6 +44,14 @@ export const shuffleArrayInPlace = (array, rnd) => {
 	}
 };
 
+const IS_STEP_EPSILON = 0.0000001;
+
+export const isStep = (value, step) => {
+	const remainder = value / step;
+	const nonIntegerRemainder = remainder - (Math.round(remainder));
+	return nonIntegerRemainder < IS_STEP_EPSILON;
+};
+
 export const DELETE_SENTINEL = {DELETE:true};
 
 //path is a dotted list of accessors in the object, returns a new object with

src/options.js
@@ -17,6 +17,7 @@ const ALLOWED_BEHAVIOR_NAMES = {
 };
 
 import {
+	isStep,
 	setPropertyInObject
 } from './util.js';
 
@@ -282,7 +283,7 @@ export const configObjectIsValid = (optionsConfig, value) => {
 	if (typeof value == 'number') {
 		if (optionsConfig[MIN_PROPERTY_NAME] !== undefined && optionsConfig[MIN_PROPERTY_NAME] > value) return [MIN_PROPERTY_NAME + ' was set and the value was less than it'];
 		if (optionsConfig[MAX_PROPERTY_NAME] !== undefined && optionsConfig[MAX_PROPERTY_NAME] < value) return [MAX_PROPERTY_NAME + ' was set and the value was more than it'];
-		if (optionsConfig[STEP_PROPERTY_NAME] !== undefined && !Number.isInteger(value / optionsConfig[STEP_PROPERTY_NAME])) return [STEP_PROPERTY_NAME + ' was set but the value was not a multiple of it'];
+		if (optionsConfig[STEP_PROPERTY_NAME] !== undefined && !isStep(value, optionsConfig[STEP_PROPERTY_NAME])) return [STEP_PROPERTY_NAME + ' was set but the value was not a multiple of it'];
 	}
 	//Skip null values, we already checked above if it was OK they were null.
 	if (typeof value == 'object'  && value && Array.isArray(value)) {

commit e20c4380bb7f12a14e0faa98a373cac74442be16

Fix a test that broke in 64044fb. isStep wasn't handling certain step values well. Added more tests to catch it. Part of #25.

src/util.js
@@ -48,7 +48,7 @@ const IS_STEP_EPSILON = 0.0000001;
 
 export const isStep = (value, step) => {
 	const remainder = value / step;
-	const nonIntegerRemainder = remainder - (Math.round(remainder));
+	const nonIntegerRemainder = Math.abs(remainder - (Math.round(remainder)));
 	return nonIntegerRemainder < IS_STEP_EPSILON;
 };
 

test/options/test.js
@@ -3,7 +3,8 @@
 import {
 	setPropertyInObject,
 	deepFreeze,
-	DELETE_SENTINEL
+	DELETE_SENTINEL,
+	isStep
 } from '../../src/util.js';
 
 import {
@@ -13,6 +14,33 @@ import {
 } from '../../src/options.js';
 
 import assert from 'assert';
+
+describe('isStep', () => {
+	it('handles 1.0 / 1.0', async () => {
+		const result = isStep(1.0, 1.0);
+		const golden = true;
+		assert.deepStrictEqual(result, golden);
+	});
+
+	it('handles 1.5 / 1.0', async () => {
+		const result = isStep(1.5, 1.0);
+		const golden = false;
+		assert.deepStrictEqual(result, golden);
+	});
+
+	it('handles 1.5 / 0.5', async () => {
+		const result = isStep(1.5, 0.5);
+		const golden = true;
+		assert.deepStrictEqual(result, golden);
+	});
+
+	it('handles 0.15 / 0.05', async () => {
+		//This one requires epsilon
+		const result = isStep(0.15, 0.05);
+		const golden = true;
+		assert.deepStrictEqual(result, golden);
+	});
+});
 
 describe('setPropertyInObject', () => {
 	it('handles null object', async () => {