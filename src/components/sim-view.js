jkomoros/CASsim

commit 0390fc1d8c45526d8721279c13d604b77979db57

Made it so the padding to reserve is proportional to the overal lwidth. This allows a tighter padding on smaller screens. Part of #27.

src/components/sim-view.js
@@ -89,8 +89,9 @@ import {
 
 import { PLUS_ICON } from "./my-icons.js";
 
-//Size in px that we want to allow around the visualization edge
-const VISUALIZATION_PADDING = 100;
+//Size in px that we want to allow around the visualization edge. Pixels per 100
+//px of width.
+const VISUALIZATION_PADDING = 8;
 
 const fetchData = async(filename) => {
 	let res;
@@ -261,8 +262,10 @@ class SimView extends connect(store)(PageViewElement) {
 		let availableWidth = pageRect.width - (this._configurationExpanded ? configurationRect.width : 0);
 		let availableHeight = pageRect.height;
 
-		availableWidth -= (2 * VISUALIZATION_PADDING);
-		availableHeight -= (2 * VISUALIZATION_PADDING);
+		const effectivePadding = VISUALIZATION_PADDING * (pageRect.width / 100);
+
+		availableWidth -= (2 * effectivePadding);
+		availableHeight -= (2 * effectivePadding);
 
 		const heightScale = availableHeight / this._height;
 		const widthScale = availableWidth / this._width;

commit 22fe8d365e3b730534ff0c93eb27232504eb2f1b

Make it so in screenshot mode we don't resize visualization. This fixes a bug broken in 0390fc1 where the final png/gif screenshot size output wasn't literally the configured amount. Part of #24.

src/components/sim-view.js
@@ -23,7 +23,8 @@ import {
 	updateScale,
 	updateKnownDatafiles,
 	updateKnownSimulatorNames,
-	DEFAULT_SENTINEL
+	DEFAULT_SENTINEL,
+	updateResizeVisualization
 } from "../actions/data.js";
 
 import {
@@ -75,6 +76,7 @@ window[PREVIOUS_FRAME_METHOD_VARIABLE] = () => {
 window[SETUP_METHOD_VARIABLE] = () => {
 	//This is the setup that the screenshot service will call before starting screenshots
 	store.dispatch(updateShowControls(false));
+	store.dispatch(updateResizeVisualization(false));
 	store.dispatch(updatePlayType(PLAY_TYPE_SIMULATION));
 	store.dispatch(advanceToLastFrameInConfig());
 };