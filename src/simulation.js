jkomoros/CASsim

commit 6cedcbc7a3f92bf2d3c94e5967fda14b8256e4ff

Fixed a finicky bug, first EXPOSED in fe9a1ae, where the first frame of the screenshot generated was 10000.
We were bailing early from frameIndexLegal, because maxFrameIndex had already been set to the simulator's max frame index... even though we hadn't validated that was ACTUALLY allowed. This didn't really show up in normal usage, but for screenshotting it did.

Part of #10.

src/simulation.js
@@ -108,7 +108,8 @@ const SimulationRun = class {
 		this._frames = [];
 		this._frameScores = [];
 		this._successScores = [];
-		this._maxFrameIndex = this._simulation.maxFrameIndex;
+		this._simulatorMaxFrameIndex = this._simulation.maxFrameIndex;
+		this._maxFrameIndex = Number.MAX_SAFE_INTEGER;
 		this._lastChanged = Date.now();
 	}
 
@@ -181,6 +182,7 @@ const SimulationRun = class {
 
 	_ensureFrameDataUpTo(frameIndex) {
 		if (frameIndex > this._maxFrameIndex) return;
+		if (frameIndex > this._simulatorMaxFrameIndex) frameIndex = this._simulatorMaxFrameIndex;
 		//We deliberately try to fetch one more frame than requested, so we'll
 		//see the end of the run of frames happening one index before we
 		//actually get there. This prevents a problem where we allow the UI to

commit 22350b52a2c33f7ad423c986e25027ef657900d5

Fixed a bug introduced in 6cedcbc7a3f92bf2d3c94e5967fda14b8256e4ff where if autoGenerate was false, we'd still generate all of a simulations' runs and frames. Part of #34. Part of #10.

src/simulation.js
@@ -162,7 +162,7 @@ const SimulationRun = class {
 	//null frame is known), either directly via run() or indirectly by having
 	//requested all frames up to and past the end.
 	get complete() {
-		return this._maxFrameIndex != this._simulation.maxFrameIndex;
+		return this._maxFrameIndex != Number.MAX_SAFE_INTEGER;
 	}
 	
 	//Returns 0.0 for complete failure, 1.0 for complete success, inbetween for