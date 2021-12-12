jkomoros/CASsim

commit cb138f61716f3d1c0005d62cb1e3c3cddd7b3b19

Change the format of config files.
Before it was just a top-level array of configs. However, that is a hard to extend format--what if we need to pack additional top-level information, like version number?

So move it to an object with a small number of keys and validation. This is important for #32, where there might be old versions stored that have to be detected. And #32 could use a different way of storing base configs (MAYBE).

src/config.js
@@ -1,0 +1,24 @@
+
+//This is the current version of the expected data payload. We should increment
+//this when large, breaking changes in how the data is packed are created.
+export const FORMAT_VERSION = 1;
+const VERSION_PROPERTY_NAME = 'version';
+const CONFIGS_PROPERTY_NAME = 'configs';
+
+//Given a raw JSON blob, unpacks and returns the array of different configs.
+export const unpackConfigJSON = (rawData) => {
+	if (!rawData || typeof rawData != 'object') throw new Error('Payload is not an object');
+	const version = rawData[VERSION_PROPERTY_NAME] || 1;
+	if (version > FORMAT_VERSION) throw new Error('Unknown version for data payload: ' + version);
+	return rawData[CONFIGS_PROPERTY_NAME] || [];
+};
+
+//Given configs, outputs a POJO that can be serialized and stored. Primarily
+//just stores a version.
+export const packConfigJSON = (configs) => {
+	if (!Array.isArray(configs)) throw new Error('Configs must me an array');
+	return {
+		[VERSION_PROPERTY_NAME]: FORMAT_VERSION,
+		[CONFIGS_PROPERTY_NAME]: configs,
+	};
+};

src/actions/data.js
@@ -78,9 +78,20 @@ import {
 	extractSimulatorNamesFromRawConfig
 } from '../simulation.js';
 
+import {
+	unpackConfigJSON
+} from '../config.js';
+
 const SIMULATORS_DIRECTORY = 'simulators';
 
-export const loadData = (data) => (dispatch) => {
+export const loadData = (blob) => (dispatch) => {
+	let data;
+	try {
+		data = unpackConfigJSON(blob);
+	} catch(err) {
+		console.warn('Couldn\'t load data:' + err);
+		return;
+	}
 	dispatch({
 		type: LOAD_DATA,
 		data,

src/components/sim-view.js
@@ -95,6 +95,10 @@ import {
 
 import { PLUS_ICON } from "./my-icons.js";
 
+import {
+	packConfigJSON
+} from "../config.js";
+
 //Size in px that we want to allow around the visualization edge. Pixels per 100
 //px of width.
 const VISUALIZATION_PADDING = 8;
@@ -123,9 +127,9 @@ const fetchData = async(filename) => {
 		console.warn('Couldn\'t fetch ' + path + ': ' + err);
 	}
 
-	const data = await res.json();
+	const blob = await res.json();
 
-	store.dispatch(loadData(data));
+	store.dispatch(loadData(blob));
 };
 
 class SimView extends connect(store)(PageViewElement) {
@@ -252,7 +256,7 @@ class SimView extends connect(store)(PageViewElement) {
 
 	// This is called every time something is updated in the store.
 	stateChanged(state) {
-		this._rawConfigData = selectRawConfigData(state);
+		this._rawConfigData = packConfigJSON(selectRawConfigData(state));
 		this._currentSimulation = selectCurrentSimulation(state);
 		this._currentSimulationName = this._currentSimulation ? this._currentSimulation.name : '';
 		this._dialogOpen = selectDialogOpen(state);

commit 7bc1ea691bbf5205ed3c8f5578d8115cb4510832

Fixed a bug broken in cb138f6 where giffing didn't work (because config data file changed).

screenshot.js
@@ -29,6 +29,8 @@ const DEFAULT_FRAME_DELAY = 100;
 const DEFAULT_EXTRA_FINAL_FRAME_COUNT = 0;
 const DEFAULT_REPEAT = false;
 
+//Duplicated in config.js
+const CONFIGS_PROPERTY_NAME = 'configs';
 
 const clearScreenshotsDir = () => {
 	if (fs.existsSync(SCREENSHOT_DIR)) {
@@ -168,8 +170,9 @@ const gifInfos = async () => {
 	}
 	const rawConfigData = fs.readFileSync(CONFIG_DATA_FILE);
 	const configData = JSON.parse(rawConfigData);
+	const configs = configData[CONFIGS_PROPERTY_NAME];
 	for (const name of Object.keys(result)) {
-		result[name] = {...result[name], ...configForGif(configData, name)};
+		result[name] = {...result[name], ...configForGif(configs, name)};
 	}
 	return result;
 };

src/config.js
@@ -3,6 +3,7 @@
 //this when large, breaking changes in how the data is packed are created.
 export const FORMAT_VERSION = 1;
 const VERSION_PROPERTY_NAME = 'version';
+//Recreated in screenshot.js
 const CONFIGS_PROPERTY_NAME = 'configs';
 
 //Given a raw JSON blob, unpacks and returns the array of different configs.