jkomoros/CASsim

commit 19521cf7da70b84493626ab3cac60fbe787d1d05

Made it so when you hit undo on a given path, we remove ALL modifications on the path.
before, if you nudged up a number 4 times, and then hit undo, it would go back to the last value it was. But that's weird (nothing else implies that those multiple modifications are kept).

Part of #33.

src/reducers/data.js
@@ -5,7 +5,7 @@ import {
 	UPDATE_RUN_INDEX,
 	UPDATE_FRAME_INDEX,
 	UPDATE_CURRENT_SIMULATION_CONFIG,
-	REMOVE_LAST_MODIFICATION_FOR_PATH,
+	REMOVE_MODIFICATIONS_FOR_PATH,
 	UPDATE_DIALOG_OPEN,
 	UPDATE_PLAY_TYPE,
 	UPDATE_PLAYING,
@@ -163,21 +163,10 @@ const data = (state = INITIAL_STATE, action) => {
 			...state,
 			resizeVisualization: action.resize,
 		};
-	case REMOVE_LAST_MODIFICATION_FOR_PATH:
-		let modificationFiltered = false;
-		let reversedModifications = [...state.modifications];
-		reversedModifications.reverse();
-		const filteredModifications = reversedModifications.filter(mod => {
-			if (modificationFiltered) return true;
-			if (mod.path != action.path) return true;
-			if (mod.simulationIndex != state.simulationIndex) return true;
-			modificationFiltered = true;
-			return false;
-		});
-		filteredModifications.reverse();
+	case REMOVE_MODIFICATIONS_FOR_PATH:
 		return {
 			...state,
-			modifications: filteredModifications,
+			modifications: state.modifications.filter(mod => mod.path != action.path && mod.simulationIndex != state.simulationIndex),
 		};
 	case CLEAR_MODIFICATIONS:
 		return {

src/actions/data.js
@@ -1,6 +1,6 @@
 export const LOAD_DATA = "LOAD_DATA";
 export const UPDATE_CURRENT_SIMULATION_CONFIG = "UPDATE_CURRENT_SIMULATION_CONFIG";
-export const REMOVE_LAST_MODIFICATION_FOR_PATH = 'REMOVE_LAST_MODIFICATION_FOR_PATH';
+export const REMOVE_MODIFICATIONS_FOR_PATH = 'REMOVE_MODIFICATIONS_FOR_PATH';
 export const UPDATE_FILENAME = 'UPDATE_FILENAME';
 export const UPDATE_SIMULATION_INDEX = 'UPDATE_SIMULATION_INDEX';
 export const UPDATE_RUN_INDEX = 'UPDATE_RUN_INDEX';
@@ -454,9 +454,9 @@ export const updateCurrentSimulationOptions = (path, value) => (dispatch, getSta
 	dispatch(verifyValidIndexes());
 };
 
-export const removeLastModificationForPath = (path) => {
+export const removeModificationsForPath = (path) => {
 	return {
-		type: REMOVE_LAST_MODIFICATION_FOR_PATH,
+		type: REMOVE_MODIFICATIONS_FOR_PATH,
 		path
 	};
 };

src/components/simulation-controls.js
@@ -36,7 +36,7 @@ import {
 	updatePathExpanded,
 	updateConfigurationExpanded,
 	updateDescriptionExpanded,
-	removeLastModificationForPath,
+	removeModificationsForPath,
 	updateFilename,
 	DIALOG_TYPE_JSON,
 	clearModifications
@@ -222,7 +222,7 @@ class SimulationControls extends connect(store)(LitElement) {
 	}
 
 	_handleUndoClicked(e) {
-		store.dispatch(removeLastModificationForPath(e.detail.path));
+		store.dispatch(removeModificationsForPath(e.detail.path));
 	}
 
 	_handleRemoveModificationsClicked() {

commit 547c1cd3f7a3ce83f4d6eaef3b91a111f4f78512

Fixed a bug broken in 19521cf7da70b84493626ab3cac60fbe787d1d05 where if you undid ANY path, it would undo ALL paths.
Part of #33.

src/reducers/data.js
@@ -166,7 +166,7 @@ const data = (state = INITIAL_STATE, action) => {
 	case REMOVE_MODIFICATIONS_FOR_PATH:
 		return {
 			...state,
-			modifications: state.modifications.filter(mod => mod.path != action.path && mod.simulationIndex != state.simulationIndex),
+			modifications: state.modifications.filter(mod => !(mod.path == action.path && mod.simulationIndex == state.simulationIndex)),
 		};
 	case CLEAR_MODIFICATIONS:
 		return {