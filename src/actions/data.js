jkomoros/CASsim

commit b172932809007a118c6872a1875f2277830ade77

Make it so the selectors know how to handle DEFAULT_SENTINEL to modify underlying data (but it will never receive that currently. Part of #33.

src/selectors.js
@@ -2,12 +2,17 @@ import { createSelector } from "reselect";
 
 import {
 	SimulationCollection,
-	extractSimulatorNamesFromRawConfig
+	extractSimulatorNamesFromRawConfig,
+	Simulation
 } from "./simulation.js";
 
 import {
 	setSimPropertyInConfig
 } from './options.js';
+
+import {
+	DEFAULT_SENTINEL
+} from './util.js';
 
 const selectRawConfigData = state => state.data ? state.data.data : [];
 const selectModifications = state => state.data ? state.data.modifications : [];
@@ -55,21 +60,38 @@ export const selectDescriptionExpanded = createSelector(
 	(showControls, rawExpanded) => showControls && rawExpanded
 );
 
-export const selectConfigData = createSelector(
+const modfifiedConfigData = (rawConfigData, modifications, simulatorsLoaded = true, simulatorNames = [], expandDefault = false) => {
+	if (!simulatorsLoaded) return rawConfigData;
+	let data = rawConfigData;
+	for (const modification of modifications) {
+		data = [...data];
+		let value = modification.value;
+		if (value == DEFAULT_SENTINEL) {
+			if (!expandDefault) {
+				continue;
+			}
+			try {
+				const simulation = new Simulation(data[modification.simulationIndex], 0, simulatorNames);
+				value = simulation.defaultValueForOptionsPath(modification.path);
+			} catch(err) {
+				console.warn('Couldn\'t fetch default value from simulator: ' + err);
+				return data;
+			}
+		}
+		data[modification.simulationIndex] = setSimPropertyInConfig(data[modification.simulationIndex], modification.path, value);
+	}
+	return data;
+};
+
+const selectModififedConfigDataNoDefaults = createSelector(
 	selectRawConfigData,
 	selectModifications,
-	(rawConfigData, modifications) => {
-		let data = rawConfigData;
-		for (const modification of modifications) {
-			data = [...data];
-			data[modification.simulationIndex] = setSimPropertyInConfig(data[modification.simulationIndex], modification.path, modification.value);
-		}
-		return data;
-	}
+	(rawConfigData, modifications) => modfifiedConfigData(rawConfigData, modifications)
 );
 
 const selectRequiredSimulatorsLoaded = createSelector(
-	selectConfigData,
+	//We do want to see if there are modifications to change sim on any config item, but don't need any default expansion (default expansion relies on this value)
+	selectModififedConfigDataNoDefaults,
 	selectLoadedSimulators,
 	(data, loadedSimulators) => {
 		const requiredSimulatorNames = extractSimulatorNamesFromRawConfig(data);
@@ -78,6 +100,14 @@ const selectRequiredSimulatorsLoaded = createSelector(
 		}
 		return true;
 	}
+);
+
+export const selectConfigData = createSelector(
+	selectRawConfigData,
+	selectModifications,
+	selectRequiredSimulatorsLoaded, 
+	selectKnownSimulatorNames,
+	(rawConfigData, modifications, simulatorsLoaded, simulatorNames) => modfifiedConfigData(rawConfigData, modifications, simulatorsLoaded, simulatorNames, true)
 );
 
 export const selectDataIsFullyLoaded = createSelector(

src/simulation.js
@@ -228,7 +228,7 @@ const makeSeededRandom = seed => {
 	return () => rnd.quick();
 };
 
-const Simulation = class {
+export const Simulation = class {
 	constructor(config, index, knownSimulatorNames = []) {
 
 		const name = config[NAME_PROPERTY];

commit 338a3066cc57dab07d815d23cd7aee94ea680fcf

Fixed it so modifying simulator on a config works. This was broken in b172932. Part of #33.

src/actions/data.js
@@ -65,7 +65,7 @@ import {
 	selectFrameDelay,
 	selectDelayCount,
 	selectLoadedSimulators,
-	selectConfigData,
+	selectModififedConfigDataNoDefaults,
 	selectScale,
 	selectDataIsFullyLoaded,
 	selectHasModifications,
@@ -118,7 +118,7 @@ export const loadData = (blob) => (dispatch) => {
 export const fetchNeededSimulators = () => (dispatch, getState) => {
 	const state = getState();
 	const loadedSimulators = selectLoadedSimulators(state);
-	const rawConfig = selectConfigData(state);
+	const rawConfig = selectModififedConfigDataNoDefaults(state);
 	const neededSimulatorNames = extractSimulatorNamesFromRawConfig(rawConfig);
 	for (const name of neededSimulatorNames) {
 		if (loadedSimulators[name]) continue;

src/selectors.js
@@ -86,7 +86,7 @@ const modfifiedConfigData = (rawConfigData, modifications, simulatorsLoaded = tr
 	return data;
 };
 
-const selectModififedConfigDataNoDefaults = createSelector(
+export const selectModififedConfigDataNoDefaults = createSelector(
 	selectRawConfigData,
 	selectModifications,
 	(rawConfigData, modifications) => modfifiedConfigData(rawConfigData, modifications)