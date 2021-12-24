1246c62f784b62726584725f50e09d2f18d1549e
Alex Komoroske <jkomoros@gmail.com>
2021-12-23 19:09:46 -0800

    Make dice-roll-demo a little bit better of a place to fork from. Part of #20.

--- src/simulators/dice-roll-demo.js
+++ src/simulators/dice-roll-demo.js
@@ -2,6 +2,9 @@ import {
 	BaseSimulator
 } from '../simulator.js';
 
+//Remember that the name must be the same as the filename of this file
+const SIMULATOR_NAME = 'dice-roll-demo';
+
 /*
 Sim options shape:
 
@@ -18,9 +21,8 @@ Sim options shape:
 
 class DiceRollDemoSimulator extends BaseSimulator {
 
-	//Remember that the name must be the same as the filename of this file
 	get name() {
-		return 'dice-roll-demo';
+		return SIMULATOR_NAME;
 	}
 
 	_firstFrameGenerator(simOptions) {
@@ -162,4 +164,4 @@ class DiceRollDemoRenderer extends LitElement {
 	}
 }
 
-window.customElements.define("dice-roll-demo-renderer", DiceRollDemoRenderer);
+window.customElements.define(SIMULATOR_NAME + "-renderer", DiceRollDemoRenderer);
