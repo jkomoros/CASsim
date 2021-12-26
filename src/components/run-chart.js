0ed45d94810d4c494ebf9a17bc10b9e9bc461713
Alex Komoroske <jkomoros@gmail.com>
2021-12-26 13:40:22 -0800

    Render out y ticks. Part of #22.

--- src/components/run-chart.js
+++ src/components/run-chart.js
@@ -142,7 +142,7 @@ class RunChart extends LitElement {
 			<svg viewBox='0 0 ${rect.width} ${rect.height}'>
 				<rect x='${padding}' y='0' width='${chartWidth}' height='${chartHeight}' fill-opacity='0.0'></rect>
 				${this._xTicks().map((tick, index, ticks) => svg`<path class='tick xTick' d='M${chartOriginX + tick.value * xFactor},${chartOriginY} v ${tickLength}'></path>${tick.title ? svg`<text class='label' text-anchor='${index == ticks.length - 1 ? 'end' : 'middle'}' x='${chartOriginX + tick.value * xFactor}' y='${rect.height}' font-size='${tickLength * 1.5}px'>${tick.title}</text>` : ''}`)}
-				${this._yTicks().map(tick => svg`<path class='tick yTick' d='M${chartOriginX},${chartOriginY - (tick.value * yFactor)} h ${-1.0 * tickLength}'></path>`)}
+				${this._yTicks().map((tick, index, ticks) => svg`<path class='tick yTick' d='M${chartOriginX},${chartOriginY - (tick.value * yFactor)} h ${-1.0 * tickLength}'></path>${tick.title ? svg`<text class='label' dominant-baseline='center' x='0' y='${chartOriginY - (tick.value * yFactor) + (index == ticks.length - 1 ? tickLength * 1.5 : 0)}' font-size='${tickLength * 1.5}px'>${tick.title}</text>` : ''}`)}
 				${Object.values(this.data).map(run => 
 		svg`<path class='run' stroke='${this._colorForRun(run)}' d='${run.data.map((value, index) => (index == 0 ? 'M ' : 'L ') + ((index * xFactor) + chartOriginX) + ', ' + (chartOriginY - (value * yFactor)) + ' ')}'>
 						<title>${run.config.title || run.config.id}</title>
