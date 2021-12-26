jkomoros/CASsim

commit 967328cf480511a628fc84715b1eb0bf6fa41dcf

Wire through chartSingleRun action creator and selector. Part of #22.

src/selectors.js
@@ -40,6 +40,7 @@ export const selectLoadedSimulators = state => state.data ? state.data.loadedSim
 export const selectKnownDatafiles = state => state.data ? state.data.knownDatafiles : [];
 export const selectKnownSimulatorNames = state => state.data ? state.data.knownSimulatorNames : [];
 export const selectResizeVisualization = state => state.data ? state.data.resizeVisualization : false;
+export const selectChartSingleRun = state => state.data ? state.data.chartSinglerRun : false;
 
 export const selectDialogOpen = state => state.data ? state.data.dialogOpen : false;
 export const selectDialogType = state => state.data ? state.data.dialogType : '';

src/actions/data.js
@@ -25,6 +25,7 @@ export const SIMULATION_CHANGED = 'SIMULATION_CHANGED';
 export const UPDATE_SCREENSHOTTING = 'UPDATE_SCREENSHOTTING';
 export const UPDATE_HASH = 'UPDATE_HASH';
 export const UPDATE_WARNING = 'UPDATE_WARNING';
+export const UPDATE_CHART_SINGLE_RUN = 'UPDATE_CHART_SINGLE_RUN';
 
 export const DIALOG_TYPE_JSON = 'json';
 export const DIALOG_TYPE_ADD_FIELD = 'add-field';
@@ -648,5 +649,12 @@ export const updateWarning = (message) => {
 	return {
 		type: UPDATE_WARNING,
 		message
+	};
+};
+
+export const updateChartSingleRun = (singleRun) => {
+	return {
+		type: UPDATE_CHART_SINGLE_RUN,
+		singleRun,
 	};
 };

src/reducers/data.js
@@ -26,6 +26,7 @@ import {
 	UPDATE_SCREENSHOTTING,
 	UPDATE_HASH,
 	UPDATE_WARNING,
+	UPDATE_CHART_SINGLE_RUN,
 
 	DIALOG_TYPE_JSON,
 	DEFAULT_FILE_NAME,
@@ -64,6 +65,7 @@ const INITIAL_STATE = {
 	chartExpanded: false,
 	configurationExpanded: false,
 	descriptionExpanded: true,
+	chartSingleRun: false,
 	//The options-paths whose "Advanced" zippy should be shown as expanded. ''
 	//is the top level 
 	pathExpanded: {},
@@ -225,6 +227,11 @@ const data = (state = INITIAL_STATE, action) => {
 		return {
 			...state,
 			warning: action.message
+		};
+	case UPDATE_CHART_SINGLE_RUN:
+		return {
+			...state,
+			chartSingleRun: action.singleRun,
 		};
 	default:
 		return state;

commit 15067405a0e3f0957bb2947293c6bd6f3bdaee53

Fixed a bug broken in 967328cf480511a628fc84715b1eb0bf6fa41dcf where selectChartSingleRun selected the wrong value. Part of #22.

src/selectors.js
@@ -40,7 +40,7 @@ export const selectLoadedSimulators = state => state.data ? state.data.loadedSim
 export const selectKnownDatafiles = state => state.data ? state.data.knownDatafiles : [];
 export const selectKnownSimulatorNames = state => state.data ? state.data.knownSimulatorNames : [];
 export const selectResizeVisualization = state => state.data ? state.data.resizeVisualization : false;
-export const selectChartSingleRun = state => state.data ? state.data.chartSinglerRun : false;
+export const selectChartSingleRun = state => state.data ? state.data.chartSingleRun : false;
 
 export const selectDialogOpen = state => state.data ? state.data.dialogOpen : false;
 export const selectDialogType = state => state.data ? state.data.dialogType : '';