d5db75efb94da69b90cdaf6b09bae05f32a207a0
Alex Komoroske <jkomoros@gmail.com>
2021-12-27 14:48:35 -0800

    Make a RectangleGraphRenderer stub. Part of #20. Part of #30.

--- src/renderer.js
+++ src/renderer.js
@@ -1,6 +1,10 @@
 
 import { LitElement, html, css} from "lit-element";
 
+import {
+	RectangleGraph
+} from './graph.js';
+
 export class BaseRenderer extends LitElement {
 	static get properties() {
 		return {
@@ -51,4 +55,42 @@ export class BaseRenderer extends LitElement {
 		return html`<pre>${JSON.stringify(this.frame, '', 2)}</pre>`;
 	}
 
+}
+
+export class RectangleGraphRenderer extends BaseRenderer {
+	
+	static get styles() {
+		return [
+			BaseRenderer.styles,
+			css`
+				.node {
+					position: absolute;
+					height: var(--node-size);
+					width: var(--node-size);
+					background-color: var(--primary-color);
+					border: 1px solid black;
+				}
+			`
+		];
+	}
+	
+	//This is an override point for your renderer to tell the renderer where the rectangle graph data is
+	rectangleGraphData(frame) {
+		return frame.graph;
+	}
+
+	innerRender() {
+		//TODO: calculate this based on rows/cols of graph and size
+		const size = 50;
+		const data = this.rectangleGraphData(this.frame);
+		if (!data) return html`<em>Loading...</em>`;
+		const graph = new RectangleGraph(data);
+		return html`
+			<style>
+				:host {
+					--node-size: ${size}px;
+				}
+			</style>
+			${Object.values(graph.nodes()).map(nodeValues => html`<div class='node' style=${'left:' + nodeValues.col * size + 'px; top:' + nodeValues.row * size + 'px;'}></div>`)}`;
+	}
 }
