8ee72fc7e41355ea97f8a1097a2c689789e81e64
Alex Komoroske <jkomoros@gmail.com>
2021-12-27 16:01:09 -0800

    Document AgentSimulator and override points. Part of #30.

--- src/agent-simulator.js
+++ src/agent-simulator.js
@@ -4,15 +4,26 @@ import { RectangleGraph }from './graph.js';
 
 export class AgentSimulator extends BaseSimulator {
 
+	/*
+		An override point if you use change the kind of graph in use.
+	*/
 	graphConstructor() {
 		return RectangleGraph;
 	}
 
+	/*
+		An override point for your generateFirstFrame. You should return the
+		graph that agents will traverse. You might also want to override
+		graphConstructor.
+	*/
 	//eslint-disable-next-line
 	generateGraph(simOptions, rnd) {
 		return RectangleGraph.make(simOptions.rows, simOptions.cols);
 	}
 
+	/*
+		Emit your starter set of agents.
+	*/
 	generateAgents(graph, simOptions, rnd) {
 		const agents = [];
 		const nodes = Object.keys(graph.nodes());
@@ -25,6 +36,9 @@ export class AgentSimulator extends BaseSimulator {
 		return agents;
 	}
 
+	/* 
+		Create the graph and the agents
+	*/
 	generateFirstFrame(simOptions, rnd) {
 		//The default generator will expand this with index and simOptions.
 		const graph = this.generateGraph(simOptions, rnd);
@@ -35,11 +49,19 @@ export class AgentSimulator extends BaseSimulator {
 		};
 	}
 
+	/*
+		Called on every frame for every agent. Return the new agent to store in
+		frame. If no modifications you can return the agent as is. IF
+		modifications, make a modified copy of agent and return that.
+	*/
 	//eslint-disable-next-line
 	agentTick(agent, graph, frame, rnd) {
 		return agent;
 	}
 
+	/*
+		Ticks all agents 
+	*/
 	generateFrame(frame, rnd) {
 		const graph = new (this.graphConstructor())(frame.graph);
 		const newAgents = [];
