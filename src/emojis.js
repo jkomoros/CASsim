b88d22463646b2729aa87bed616c8e900b61c059
Alex Komoroske <jkomoros@gmail.com>
2021-12-29 10:00:44 -0800

    Add grazing animal emojis. Part of #30. Part of #13.

--- src/emojis.js
+++ src/emojis.js
@@ -16,3 +16,16 @@ export const PROFESSIONAL_PEOPLE_EMOJIS = {
 export const PEOPLE_EMOJIS = {
 	...PROFESSIONAL_PEOPLE_EMOJIS
 };
+
+export const GRAZING_FARM_ANIMALS_EMOJIS = {
+	'cow': '🐄',
+	'water-buffalo': '🐃',
+	'ox': '🐂',
+	'ram': '🐏',
+	'sheep': '🐑',
+	'goat': '🐐'
+};
+
+export const ANIMAL_EMOJIS = {
+	...GRAZING_FARM_ANIMALS_EMOJIS,
+};
