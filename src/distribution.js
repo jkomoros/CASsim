3c5b7df499776d3300597f18e5a5000ce3ecb4ff
Alex Komoroske <jkomoros@gmail.com>
2022-01-02 08:03:41 -0800

    Stubs of Distribution, DistributionConfig, LinearDistributionConfig. Part of #48.

--- src/distribution.js
+++ src/distribution.js
@@ -8,3 +8,76 @@ export const linearMinMaxDistribution = (min, max, rnd = Math.random, clipMin = 
 	if (result > clipMax) return clipMax;
 	return result;
 };
+
+export const LINEAR = 'linear';
+
+const LEGAL_TYPES = {
+	[LINEAR]: true
+};
+
+//Get a new one from DistributionConfig.distribution()
+class Distribution {
+	constructor(options = {}) {
+		//Already validated by DistributionConfig.
+		this._options = options;
+	}
+
+	sample(rnd = Math.random) {
+		//TODO: do different things based on type;
+		let value = (this._options.max - this._options.min) * rnd() + this._options.min;
+		if (value < this._options.min) value = this._options.min;
+		if (value > this._options.max) value = this._options.max;
+		return value;
+	}
+}
+
+/*
+	options includes:
+	- types: an array of allowed types (e.g. LINEAR)
+	- type: the default type
+	- average: the default average value
+	- spread: the spread value
+	- min: the clip value. Defaults to 0.0.
+	- max: the clip value. Defaults to 1.0.
+	- step: defaults to 0.01
+*/
+export class DistributionConfig {
+	constructor(options = {}) {
+		const normalizedOptions = {};
+		normalizedOptions.types = options.types || (options.type ? [options.type] : [LINEAR]);
+		normalizedOptions.type = options.type || normalizedOptions.types[0];
+		normalizedOptions.average = options.average === undefined ? 0.5 : options.average;
+		normalizedOptions.spread = options.spread === undefined ? 0.0 : options.spread;
+		normalizedOptions.min = options.min === undefined ? 0.0 : options.min;
+		normalizedOptions.max = options.max === undefined ? 1.0 : options.max;
+		normalizedOptions.step = options.step === undefined ? 0.01 : options.step;
+
+		//Validate
+		this._validateOptions(normalizedOptions);
+
+		this._options = normalizedOptions;
+	}
+
+	distribution(overrides = {}) {
+		const options = {...this._options, ...overrides};
+		this._validateOptions(options);
+		return new Distribution(options);
+	}
+
+	_validateOptions(normalizedOptions) {
+		for (const key of ['average', 'spread', 'min', 'max', 'step']) {
+			if (typeof normalizedOptions[key] != 'number') throw new Error(key + ' must be a number');
+		}
+		if (normalizedOptions.min > normalizedOptions.max) throw new Error('min was greater than max');
+		for (const type of normalizedOptions.types) {
+			if (!LEGAL_TYPES[type]) throw new Error(type + ' is not a legal type');
+		}
+		if (!normalizedOptions.types.some(type => type == normalizedOptions.type)) throw new Error(normalizedOptions.type + ' was set as type but was not in types');
+	}
+}
+
+export class LinearDistributionConfig extends DistributionConfig {
+	constructor(options = {}) {
+		super({...options, types: [LINEAR], type: LINEAR});
+	}
+}
