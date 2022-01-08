jkomoros/CASsim

commit fa8aaf7e574a2d15d2f6aa3fd3b1fb56539dd6a5

Make it so knownSimulatorNames isn't actually used in Simulation or SimulationCollection.
It's a BIT weird, because technically we're relying on state outside of the selectors, but it's only used for the optionsConfig anyway so it's not that big of a deal, and this makes it easier to create a new Simulation faster. Part of #49.

src/simulation.js
@@ -77,7 +77,7 @@ export const extractSimulatorNamesFromModifications = modifications => {
 };
 
 export const SimulationCollection = class {
-	constructor(configs, knownSimulatorNames, unmodifiedConfigs) {
+	constructor(configs, unmodifiedConfigs) {
 		if (!configs) configs = [];
 		if (!unmodifiedConfigs) unmodifiedConfigs = [];
 		const seenNames = {};
@@ -86,7 +86,7 @@ export const SimulationCollection = class {
 			let sim;
 			const config = configs[i];
 			try {
-				sim = new Simulation(config, i, knownSimulatorNames, unmodifiedConfigs[i]);
+				sim = new Simulation(config, i, unmodifiedConfigs[i]);
 			} catch(err) {
 				throw new Error('Config #' + i + ' errored: ' + err);
 			}
@@ -248,7 +248,7 @@ const makeSeededRandom = seed => {
 };
 
 export const Simulation = class {
-	constructor(config, index, knownSimulatorNames = [], unmodifiedConfig) {
+	constructor(config, index, unmodifiedConfig) {
 
 		const name = config[NAME_PROPERTY];
 		if (name) {
@@ -259,7 +259,6 @@ export const Simulation = class {
 		if (!this._simulator) {
 			throw new Error('Unknown simulator name: ' + config.sim);
 		}
-		this._knownSimulatorNames = knownSimulatorNames;
 		const configCopy = deepCopy(config);
 		const rawSimOptions = configCopy[SIM_OPTIONS_PROPERTY] || this._simulator.defaultValueForPath('', null);
 		const [updatedSimOptionsConfig] = ensureBackfill(this._simulator.optionsConfig, rawSimOptions);
@@ -303,11 +302,7 @@ export const Simulation = class {
 	}
 
 	cloneWithConfig(config) {
-		return new Simulation(config, this.index, this.knownSimulatorNames, config);
-	}
-
-	get knownSimulatorNames() {
-		return this._knownSimulatorNames;
+		return new Simulation(config, this.index, config);
 	}
 
 	get lastChanged() {
@@ -620,9 +615,9 @@ export const Simulation = class {
 				},
 				[SIM_PROPERTY]: {
 					//TODO: use the constant
-					example: this._knownSimulatorNames.length ? this._knownSimulatorNames[0] : '',
+					example: Object.keys(SIMULATORS).length ? Object.keys(SIMULATORS)[0] : '',
 					shortName: SIM_PROPERTY_SHORT_NAME,
-					options: this._knownSimulatorNames.map(name => ({value: name})),
+					options: Object.keys(SIMULATORS).map(name => ({value: name})),
 					description: 'The simulator type to run. Only simulators in the simulators directory are supported',
 					//Advanced while this is the only option
 					advanced: true,

src/selectors.js
@@ -78,7 +78,7 @@ export const selectChartExpanded = createSelector(
 	(showControls, rawExpanded) => showControls && rawExpanded
 );
 
-const modfifiedConfigData = (rawConfigData, modifications, simulatorsLoaded = true, simulatorNames = [], expandDefault = false) => {
+const modfifiedConfigData = (rawConfigData, modifications, simulatorsLoaded = true, expandDefault = false) => {
 	if (!simulatorsLoaded) return rawConfigData;
 	let data = rawConfigData;
 	for (const modification of modifications) {
@@ -90,7 +90,7 @@ const modfifiedConfigData = (rawConfigData, modifications, simulatorsLoaded = tr
 				continue;
 			}
 			try {
-				const simulation = new Simulation(data[modification.simulationIndex], 0, simulatorNames);
+				const simulation = new Simulation(data[modification.simulationIndex], 0);
 				value = simulation.defaultValueForOptionsPath(modification.path);
 			} catch(err) {
 				console.warn('Couldn\'t fetch default value from simulator: ' + err);
@@ -140,7 +140,8 @@ export const selectConfigData = createSelector(
 	selectModifications,
 	selectRequiredSimulatorsLoaded, 
 	selectKnownSimulatorNames,
-	(rawConfigData, modifications, simulatorsLoaded, simulatorNames) => modfifiedConfigData(rawConfigData, modifications, simulatorsLoaded, simulatorNames, true)
+	//We don't actually pass knownSimulatorNames, we just need to retickle this when they change
+	(rawConfigData, modifications, simulatorsLoaded) => modfifiedConfigData(rawConfigData, modifications, simulatorsLoaded, true)
 );
 
 export const selectDataIsFullyLoaded = createSelector(
@@ -152,9 +153,10 @@ export const selectDataIsFullyLoaded = createSelector(
 export const selectSimulationCollection = createSelector(
 	selectConfigData,
 	selectRequiredSimulatorsLoaded,
-	selectKnownSimulatorNames,
 	selectRawConfigData,
-	(rawConfig, simulatorsLoaded, knownSimulatorNames, unmodifiedConfigData) => simulatorsLoaded ? new SimulationCollection(rawConfig, knownSimulatorNames, unmodifiedConfigData) : null
+	selectKnownSimulatorNames,
+	//We don't actually pass knownSimulatorNames, we just need to retickle this when they change
+	(rawConfig, simulatorsLoaded, unmodifiedConfigData) => simulatorsLoaded ? new SimulationCollection(rawConfig, unmodifiedConfigData) : null
 );
 
 export const selectURLDiffHash = createSelector(

commit cb188df3d839b2ba88cb88b19b3bf382b4892d92

Fix the bug introduced in fa8aaf7 (where simulator names wouldn't show up in the drop down) with an ugly, redux-store-evading hack.
but at least it's documented why and how it works. Part of #49.

src/actions/data.js
@@ -92,6 +92,7 @@ import {
 import { store } from '../store.js';
 
 import {
+	KNOWN_SIMULATOR_NAMES,
 	SIMULATORS,
 } from '../simulation.js';
 
@@ -580,6 +581,15 @@ export const updateKnownSimulatorNames = (simulatorNames) => (dispatch) => {
 	if (!Array.isArray(simulatorNames)) {
 		console.warn('simulatorsNames is not an array');
 		return;
+	}
+	//Yes, this is bad, bad, bad. I don't want to pass around
+	//knownSimulatorNames from state into Simulation all of the time (we have to
+	//clone them a bunch in random places) for no good reason, so we'll store
+	//them on the side for the drop down. We already re-generate them when this
+	//changes by having the selectors invalidate when knownSimulatorNames
+	//changes, even though we don't pass it to the SimulationCollection/Simulation constructors.
+	for (const name of simulatorNames) {
+		KNOWN_SIMULATOR_NAMES[name] = true;
 	}
 	dispatch({
 		type: UPDATE_KNOWN_SIMULATOR_NAMES,

src/simulation.js
@@ -58,6 +58,7 @@ const DEFAULT_EXTRA_FINAL_FRAME_COUNT = 0;
 const DEFAULT_REPEAT = false;
 
 export const SIMULATORS = {};
+export const KNOWN_SIMULATOR_NAMES = {};
 
 //Returns an array of the names of simulators in this data blob.
 export const extractSimulatorNamesFromRawConfig = data => {
@@ -640,9 +641,9 @@ export const Simulation = class {
 				},
 				[SIM_PROPERTY]: {
 					//TODO: use the constant
-					example: Object.keys(SIMULATORS).length ? Object.keys(SIMULATORS)[0] : '',
+					example: Object.keys(KNOWN_SIMULATOR_NAMES).length ? Object.keys(SIMULATORS)[0] : '',
 					shortName: SIM_PROPERTY_SHORT_NAME,
-					options: Object.keys(SIMULATORS).map(name => ({value: name})),
+					options: Object.keys(KNOWN_SIMULATOR_NAMES).map(name => ({value: name})),
 					description: 'The simulator type to run. Only simulators in the simulators directory are supported',
 					//Advanced while this is the only option
 					advanced: true,