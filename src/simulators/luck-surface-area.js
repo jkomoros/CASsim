211496315d60d81d40b8f0f65e14d7255c3615c8
Alex Komoroske <jkomoros@gmail.com>
2022-01-09 07:48:00 -0800

    Check in an initial stub of luck-surface-area sim. The graph doesn't layout yet so you can't really see what's happening. Part of #30. Part of #46.

---
+++ src/simulators/luck-surface-area.js
@@ -0,0 +1,77 @@
+import {
+	AgentSimulator
+} from '../agent-simulator.js';
+
+import {
+	PositionedGraph
+}from '../graph.js';
+
+//Remember that the name must be the same as the filename of this file
+const SIMULATOR_NAME = 'luck-surface-area';
+
+class AgentDemoSimulator extends AgentSimulator {
+
+	get name() {
+		return SIMULATOR_NAME;
+	}
+
+	//We use the default generator, which will call generateFirstFrame,
+	//simulationComplete, and generateFrame.
+
+	generateGraph(simOptions, rnd, simWidth, simHeight) {
+		return PositionedGraph.makeBloomGraph(simWidth, simHeight);
+	}
+
+	numStarterAgents(graph, simOptions) {
+		return simOptions.agents;
+	}
+
+	simulationComplete(frame) {
+		return frame.index >= frame.simOptions.rounds;
+	}
+
+	frameScorer(frame) {
+		const finalScore = this.simulationComplete(frame) ? 1.0 : -1;
+		return [finalScore, Object.keys(frame.agents).length];
+	}
+
+	scoreConfig() {
+		return [
+			null,
+			{
+				id:'agent-count'
+			}
+		];
+	}
+	
+	get optionsConfig() {
+		return {
+			'agents': {
+				example: 6,
+				optional: true,
+				backfill: true,
+				default: true,
+				shortName: 'a',
+				description: 'The number of agents',
+			},
+			'rounds': {
+				example: 15,
+				optional: true,
+				backfill: true,
+				default: true,
+				shortName: 'n',
+				description: 'The number of rounds'
+			}
+		};
+	}
+
+	renderer() {
+		return new PositionedGraphRenderer();
+	}
+}
+
+export default AgentDemoSimulator;
+
+import { PositionedGraphRenderer } from '../renderer.js';
+
+window.customElements.define(SIMULATOR_NAME + "-renderer", PositionedGraphRenderer);
