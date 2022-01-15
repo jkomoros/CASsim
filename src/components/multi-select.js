4f210cb16c8ea9e2da83c1988ef7feecd0603749
Alex Komoroske <jkomoros@gmail.com>
2022-01-15 09:48:17 -0800

    Swap out the select into a list of rendered checkboxes with radio behavior. Styling is a little weird. Part of #54.

--- src/components/multi-select.js
+++ src/components/multi-select.js
@@ -1,4 +1,5 @@
 import { LitElement, html, css } from "lit-element";
+import { SharedStyles } from './shared-styles.js';
 
 class MultiSelect extends LitElement {
 	static get properties() {
@@ -12,7 +13,12 @@ class MultiSelect extends LitElement {
 
 	static get styles() {
 		return [
-			css``
+			SharedStyles,
+			css`
+				:host {
+					display: inline-block;
+				}
+			`
 		];
 	}
 
@@ -20,12 +26,18 @@ class MultiSelect extends LitElement {
 		const defaultText = this.defaultText || 'None';
 		//If there's a single option, then 'All' doesn't have any meaning so only render that one
 		const options = Object.keys(this.options).length == 1 ? this.options : {'': defaultText, ...this.options};
-		return html`<select @change=${this._handleSelectChanged} .disabled=${this.disabled}>
-			${Object.entries(options).map(entry => html`<option .value=${entry[0]} .selected=${entry[0] == this.value}>${typeof entry[1] == 'string' && entry[1] ? entry[1] : entry[0]}</option>`)}
-		</select>`;
+		//Fill in display values
+		const defaultedOptions = Object.fromEntries(Object.entries(options).map(entry => [entry[0], typeof entry[1] == 'string' && entry[1] ? entry[1] : entry[0]]));
+		const summary = Object.keys(defaultedOptions).length == 1 ? Object.values(defaultedOptions)[0] : defaultedOptions[this.value];
+		return html`<details>
+			<summary><label>${summary}</label></summary>
+			${Object.entries(defaultedOptions).map((entry, index, entries) => html`<input id=${entry[0]} type='checkbox' .disabled=${this.disabled} .checked=${entry[0] == this.value || entries.length == 1} .value=${entry[0]} @change=${this._handleCheckboxChanged}></input><label for=${entry[0]}>${entry[1]}</label>`)}
+		</details>
+		`;
+
 	}
 
-	_handleSelectChanged(e) {
+	_handleCheckboxChanged(e) {
 		const ele = e.composedPath()[0];
 		this.value = ele.value;
 		this.dispatchEvent(new CustomEvent('change', {composed: true, detail: {value:ele.value}}));
