7407e244b4349012396a95af1491f5b98be30982
Alex Komoroske <jkomoros@gmail.com>
2022-06-22 15:13:27 +0200

    Use AppState in reducers/app.ts
    Part of #71.

--- src/reducers/app.ts
+++ src/reducers/app.ts
@@ -1,17 +1,11 @@
-/**
-@license
-Copyright (c) 2018 The Polymer Project Authors. All rights reserved.
-This code may only be used under the BSD style license found at http://polymer.github.io/LICENSE.txt
-The complete set of authors may be found at http://polymer.github.io/AUTHORS.txt
-The complete set of contributors may be found at http://polymer.github.io/CONTRIBUTORS.txt
-Code distributed by Google as part of the polymer project is also
-subject to an additional IP rights grant found at http://polymer.github.io/PATENTS.txt
-*/
-
 import {
 	UPDATE_PAGE,
 	UPDATE_OFFLINE,
 } from "../actions/app.js";
+
+import {
+	AppState
+} from '../types.js';
 
 const INITIAL_STATE = {
 	page: "",
@@ -19,7 +13,7 @@ const INITIAL_STATE = {
 	offline: false,
 };
 
-const app = (state = INITIAL_STATE, action) => {
+const app = (state : AppState = INITIAL_STATE, action) : AppState => {
 	switch (action.type) {
 	case UPDATE_PAGE:
 		return {
