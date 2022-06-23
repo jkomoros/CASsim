jkomoros/CASsim

commit 18dd1e5da8d64c0046b93059f552e26dfc1b9a80

Bail earlier from optionsLeafValidator for Arrays and OptionsConfigMap
This changes behavior just a bit... but before it was likely subtly broken. This also helps the typechecker know that the last checks in that function are only for OptionsConfigMap.

Part of #71.

src/options.ts
@@ -107,6 +107,7 @@ const optionsLeafValidator = (config : OptionsConfigExample) : string => {
 			if (problem) {
 				return "example's array first item didn't validate: " + problem;
 			}
+			return '';
 		}
 		//shortNames also may not conflict with any non-short name
 		const shortNameMap = Object.fromEntries(Object.keys(example).map(key => [key, true]));
@@ -124,6 +125,11 @@ const optionsLeafValidator = (config : OptionsConfigExample) : string => {
 			}
 		}
 	}
+	
+	//Remaining processing is for OptionsConfig only.
+	if (Array.isArray(config) || configIsMap(config)) return '';
+
+	//TODO: switch to modifying these diretly to get type checking
 
 	if (config[DESCRIPTION_PROPERTY_NAME] !== undefined && typeof config[DESCRIPTION_PROPERTY_NAME] != 'string') return DESCRIPTION_PROPERTY_NAME + ' must be a string if provided';
 	if (config[OPTIONAL_PROPERTY_NAME] !== undefined && typeof config[OPTIONAL_PROPERTY_NAME] != 'boolean') return OPTIONAL_PROPERTY_NAME + ' must be a boolean if provided';

commit 362b29e10036c2a86bb39c1e83d00349de671b2c

Fixed a bug introduced in 18dd1e5da8d64c0046b93059f552e26dfc1b9a80 that caused tests to fail.
Part of #71.

src/options.ts
@@ -111,7 +111,6 @@ const optionsLeafValidator = (config : OptionsConfigExample) : string => {
 			if (problem) {
 				return "example's array first item didn't validate: " + problem;
 			}
-			return '';
 		}
 		//shortNames also may not conflict with any non-short name
 		const shortNameMap = Object.fromEntries(Object.keys(example).map(key => [key, true]));