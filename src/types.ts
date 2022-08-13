6c815dd62e0935eeaa1137a8e3985aa4993ce2b9
Alex Komoroske <jkomoros@gmail.com>
2022-08-13 15:19:48 -0700

    Redefine OptionsConfigExample to not allow a naked OptionsConfig, and redefine OptionsConfigInput to not allow an OptionsConfigExample.
    Trade off 2 typescript warnings for another two, but I do think this is closer to being right...
    
    Part of #71.

--- src/types.ts
+++ src/types.ts
@@ -147,9 +147,9 @@ export type OptionValueMap = {
     [key : string]: OptionValue
 };
 
-export type OptionsConfigInput = OptionsConfigExample | OptionsConfigMap;
+export type OptionsConfigInput = OptionsConfig | OptionsConfigMap;
 
-export type OptionsConfigExample = number | boolean | string | [OptionsConfig] | OptionsConfig | OptionsConfigMap;
+export type OptionsConfigExample = number | boolean | string | [OptionsConfig] | OptionsConfigMap;
 
 export type OptionsConfig = {
     //Example is the most important property and the only reserved word. If an object in the config has 
