9bee27f72b48ac78d36557892a1cefa361da3615
Alex Komoroske <jkomoros@gmail.com>
2022-09-08 13:58:32 -1000

    Generate an extraction script (but don't yet actually run it).
    Part of #73.

--- tools/types.ts
+++ tools/types.ts
@@ -1,5 +1,6 @@
 import * as fs from "fs";
 import * as path from "path";
+import { OptionsConfig } from "../src/types.js";
 
 const SIMULATORS_DIR = 'src/simulators';
 const TYPES_DIR = path.join(SIMULATORS_DIR, 'types');
@@ -23,8 +24,26 @@ const makeOptionsConfigCache = () => {
 		if (stats.isDirectory()) continue;
 		if (!file.endsWith('.ts')) continue;
 		if (file.endsWith('.d.ts')) continue;
-		console.log('Found ' + file);
+		const config = extractOptionsConfigForSimulator(file);
+		//TODO: store this.
+		console.log(file, config);
 	}
+};
+
+const GENERATED_EXTRACTION_FILE = path.join(TYPES_CACHE_DIR, 'extraction.TEMP.ts');
+
+const extractOptionsConfigForSimulator = (simulatorFile : string) : OptionsConfig => {
+	const filePath = path.join('..', '..', simulatorFile.split('.ts').join('.js'));
+	const fileContents = `import Simulator from '${filePath}';
+
+const sim = new Simulator();
+console.log(sim.optionsConfig);`;
+
+	fs.writeFileSync(GENERATED_EXTRACTION_FILE, fileContents);
+
+	//TODO: execute the script and return the OptionsConfig as JSON.
+
+	return null;
 };
 
 (async() => {
