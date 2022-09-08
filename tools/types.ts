0f618dcaa73b1e141aaebd83970105ba8f4103a6
Alex Komoroske <jkomoros@gmail.com>
2022-09-08 13:37:20 -1000

    Make generate:types clear the `src/simulators/types/cache` folder or create it if it doesn't exist.
    Part of #73.

--- tools/types.ts
+++ tools/types.ts
@@ -1,2 +1,19 @@
+import * as fs from "fs";
+import * as path from "path";
 
-console.log('Hello, world');
+const TYPES_CACHE_DIR = 'src/simulators/types/cache';
+
+const clearTypesCacheDir = () => {
+	if (fs.existsSync(TYPES_CACHE_DIR)) {
+		const files = fs.readdirSync(TYPES_CACHE_DIR);
+		for (const file of files) {
+			fs.unlinkSync(path.join(TYPES_CACHE_DIR, file));
+		}
+	} else {
+		fs.mkdirSync(TYPES_CACHE_DIR);
+	}
+};
+
+(async() => {
+	clearTypesCacheDir();
+})();
