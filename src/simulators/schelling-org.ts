jkomoros/CASsim

commit bd396df7ff07d43f3aab52dbc1a7c907d770f1f2

Switch to fully generated types/schelling-org.
Part of #76.

src/simulators/schelling-org.ts
@@ -604,6 +604,10 @@ class SchellingOrgSimulator extends BaseSimulator {
 		//When you modify this method, re-run `npm run generate` to update the types and schema checking
 		return {
 			display: {
+				typeInfo: {
+					typeName: 'DisplayValue',
+					exported: true
+				},
 				example: {
 					debug: {
 						example: true,
@@ -702,6 +706,10 @@ class SchellingOrgSimulator extends BaseSimulator {
 						example: COMMUNICATION_STRATEGY_RANDOM,
 						shortName: SHORT_NAMES.communicationStrategy || '',
 						description: 'The communication strategy the individual will use when deciding which project to communicate about',
+						typeInfo: {
+							typeName: 'CommunicationStrategy',
+							exported: true
+						},
 						options: [
 							{
 								value: COMMUNICATION_STRATEGY_RANDOM,
@@ -724,6 +732,9 @@ class SchellingOrgSimulator extends BaseSimulator {
 						backfill: true,
 					},
 					randomIndividual: {
+						typeInfo: {
+							typeName: 'CollaboratorIndividualOptions'
+						},
 						example: {
 							beliefs: {
 								example: [
@@ -827,6 +838,9 @@ class SchellingOrgSimulator extends BaseSimulator {
 						backfill: true,
 						example: [
 							{
+								typeInfo: {
+									typeName: 'CollaboratorIndividualOptions'
+								},
 								example: {
 									beliefs: {
 										example: [
@@ -967,6 +981,9 @@ class SchellingOrgSimulator extends BaseSimulator {
 						description: "After a value is set for each project, twiddle it up or down by a random amount beteen 0.0 and this number."
 					},
 					randomIndividual: {
+						typeInfo: {
+							typeName: 'ProjectIndividualOptions'
+						},
 						example: {
 							marked: {
 								example: false,
@@ -1017,6 +1034,9 @@ class SchellingOrgSimulator extends BaseSimulator {
 					individuals: {
 						example: [
 							{
+								typeInfo: {
+									typeName: 'ProjectIndividualOptions'
+								},
 								example: {
 									marked: {
 										example: false,
@@ -1078,6 +1098,10 @@ class SchellingOrgSimulator extends BaseSimulator {
 				description: "Information on projects"
 			},
 			northStar: {
+				typeInfo: {
+					typeName: 'NorthStarOptions',
+					exported: true
+				},
 				example: {
 					emoji: {
 						example: DEFAULT_NORTH_STAR_EMOJI,

src/simulators/types/schelling-org.ts
@@ -1,69 +1,0 @@
-//This file is NOT generated by `npm run generate:types` because we have
-//multiple exported intermediary values, not just the main one.
-
-export type DisplayValue = {
-	debug?: boolean;
-	disableSelection? : boolean;
-	disableBeliefs? : boolean;
-}
-
-export type CommunicationStrategy = 'random' | 'min' | 'max' | 'disagreement';
-
-type CollaboratorIndividualOptions = {
-	beliefs?: number[]
-	epsilon?: number;
-	emoji?: string;
-	avgConnectionLikelihood?:number;
-	connectionLikelihoodSpread?: number;
-	compelling?:number;
-	broadcastLikelihood?:number;
-	optimism?:number;
-	believes?: boolean;
-	communicationStrategy?: CommunicationStrategy;
-}
-
-type ProjectIndividualOptions = {
-	marked? : boolean;
-	maxExtraValue? : number;
-	maxErrorValue? : number;
-	twiddleValueAmount? : number;
-	value? : number;
-	error? : number;
-}
-
-export type NorthStarOptions = {
-	emoji? : string;
-	offsetType? : 'manual' | 'random' | 'random-project';
-	minOffset? : number;
-	maxOffset? : number;		
-	offset? : number;
-	strength? : number;
-	spread? : number;
-	believability? : number;
-}
-
-export type SchellingOrgSimOptions = {
-	display? : DisplayValue;
-	communication? : number;
-	collaborators? : {
-		count: number;
-		epsilon?: number;
-		avgConnectionLikelihood? : number;
-		connectionLikelihoodSpread? : number;
-		compelling? : number;
-		broadcastLikelihood? : number;
-		optimism? : number;
-		communicationStrategy? : CommunicationStrategy;
-		randomIndividual?: CollaboratorIndividualOptions;
-		individuals?: (CollaboratorIndividualOptions | null)[];
-	};
-	projects?: {
-		count: number;
-		maxExtraValue?: number;
-		maxErrorValue?: number;
-		twiddleValueAmount? : number;
-		randomIndividual? : ProjectIndividualOptions;
-		individuals? : (ProjectIndividualOptions | null)[];
-	}
-	northStar? : NorthStarOptions;
-}

src/simulators/types/schelling-org.GENERATED.ts
@@ -1,0 +1,115 @@
+//This file was generated by `npm run generate:types`
+
+export type DisplayValue = {
+	//If true, then the SVG will render debug information
+	debug?: boolean;
+	//If true, then each individuals' beliefs about the value of a project will be rendered as a tick mark
+	disableBeliefs?: boolean;
+	//If true, then the line connecting each collaborator to the project they pick won't be rendered
+	disableSelection?: boolean;
+};
+
+export type CommunicationStrategy = 'random' | 'min' | 'max' | 'disagreement';
+
+type CollaboratorIndividualOptions = {
+	//The starter beliefs of this individual of the values of projects. Must be an array of the same length as number of projects
+	beliefs?: number[];
+	//The epsilon for this specific individual
+	epsilon?: number;
+	//The specific emoji
+	emoji?: string;
+	//avgConnectionLikelihood for this individual
+	avgConnectionLikelihood?: number;
+	//connectionLikelihoodSpread for this individual
+	connectionLikelihoodSpread?: number;
+	//When this person speaks to another person, how much does the receiver update their beliefs? 0.5 means the receiver would move their belief to be halfway between their previous belief and the speaker's belief
+	compelling?: number;
+	//For each time a speaker is selected to present, how likely are they are they to broadcast to multiple people, with any individual with a connection equal to or stronger than the selected connection is communicated with at once?
+	broadcastLikelihood?: number;
+	//How optimistic or pessimistic the individual is. 1.0 is extremely optimistic--every value will be at the top of the possible range. 0.0 is extremely pessimistic--every value will be at the bottom of the possible range.
+	optimism?: number;
+	//Whether this person believes in the north star or not. If they don't believe then they will not be influenced by the effect.
+	believes?: boolean;
+	//The communication strategy the individual will use when deciding which project to communicate about
+	communicationStrategy?: 'random' | 'min' | 'max' | 'disagreement';
+};
+
+type ProjectIndividualOptions = {
+	//A marked project shows up distinctively; collaborators, when deciding between two projects that look like the same value, will prefer the marked one.
+	marked?: boolean;
+	//Each project will get between 0.0 and this number randomly set on top of 1.0 for the value
+	maxExtraValue?: number;
+	//Each project will get between 0.0 and this number randomly set, which are the "error bars" for the value; its value is considered by collaborators to be somewhere within those values.
+	maxErrorValue?: number;
+	//After a value is set for each project, twiddle it up or down by a random amount beteen 0.0 and this number.
+	twiddleValueAmount?: number;
+	//Value is the height of the project, in units of 1.0 = width
+	value?: number;
+	//The error bars for this value; collaborators will consider the true value to be somewhere within value +/- this value
+	error?: number;
+};
+
+export type NorthStarOptions = {
+	//The emoji to render for the north star
+	emoji?: string;
+	offsetType?: 'manual' | 'random' | 'random-project';
+	//The lowest random offset to choose if offsetType is randomly selected
+	minOffset?: number;
+	//The highest random offset to choose if offsetType is randomly selected
+	maxOffset?: number;
+	//How far from fully at left to fully at right is the northstar? This value will be used directly if offsetType is manual, otherwise it will be set implicitly.
+	offset?: number;
+	//How strong is the north star effect?
+	strength?: number;
+	//How wide is the positive effect of the northstar affect (trading off linearly from the offset position to plus or minus by this amount)
+	spread?: number;
+	//The proportion of collaborators who will believe in this north star (will have their believes set to true).
+	believability?: number;
+};
+
+export type SchellingOrgSimOptions = {
+	//An optional object that controls how things render. If not provided, will be interpreted as though it enables no optional rendering.
+	display?: DisplayValue;
+	//How many rounds of communication should be allowed between agents before they decide. 0 is no communication and will render a line of collaborators with walls between them.
+	communication?: number;
+	//Information on the collaborators
+	collaborators?: {
+		//How many collaborators there should be
+		count: number;
+		//Project values within this amount of each other will be considered to be the same
+		epsilon?: number;
+		//We compute a range of possible connection likelihoods based on [avgConnectionLikelihood - connectionLikelihoodSpread, avgConnectionLikelihood + connectionLikelihoodSpread] Numbers below 0.0 or 1.0 will be clipped, which is a convenient way of making a lot of them drop out or be maximum strength.
+		avgConnectionLikelihood?: number;
+		//We compute a range of possible connection likelihoods based on [avgConnectionLikelihood - connectionLikelihoodSpread, avgConnectionLikelihood + connectionLikelihoodSpread] Numbers below 0.0 or 1.0 will be clipped, which is a convenient way of making a lot of them drop out or be maximum strength.
+		connectionLikelihoodSpread?: number;
+		//When each individual speaks to another, how much does the receiver update their beliefs, between their old belief and new belief? 0.5 would be moving halfway from old belief to new belief
+		compelling?: number;
+		//For each time a speaker is selected to present, how likely are they are they to broadcast to multiple people, with any individual with a connection equal to or stronger than the selected connection is communicated with at once?
+		broadcastLikelihood?: number;
+		//How optimistic or pessimistic the individual is. 1.0 is extremely optimistic--every value will be at the top of the possible range. 0.0 is extremely pessimistic--every value will be at the bottom of the possible range.
+		optimism?: number;
+		//The communication strategy the individual will use when deciding which project to communicate about
+		communicationStrategy?: CommunicationStrategy;
+		//If set, then the individuals array will be set so that a random individual has this configuration provided
+		randomIndividual?: CollaboratorIndividualOptions;
+		//individuals is set to override the computed individuals with the given properties. null values will be ignored, and keys not in the override will be left in place.
+		individuals?: (CollaboratorIndividualOptions | null)[];
+	};
+	//Information on projects
+	projects?: {
+		//How many projects there are
+		count: number;
+		//Each project will get between 0.0 and this number randomly set on top of 1.0 for the value
+		maxExtraValue?: number;
+		//Each project will get between 0.0 and this number randomly set, which are the "error bars" for the value; its value is considered by collaborators to be somewhere within those values.
+		maxErrorValue?: number;
+		//After a value is set for each project, twiddle it up or down by a random amount beteen 0.0 and this number.
+		twiddleValueAmount?: number;
+		//If provided, each run the individuals overrides will have a random individual provided precisely these overrides
+		randomIndividual?: ProjectIndividualOptions;
+		//individuals is set to override the computed individuals with the given properties. null values will be ignored, and keys not in the override will be left in place.
+		individuals?: (ProjectIndividualOptions | null)[];
+	};
+	//Information on an (optional) north star, which people will tend to pick towards
+	northStar?: NorthStarOptions;
+};

src/types-simulator.GENERATED.ts
@@ -21,7 +21,7 @@ import {
 
 import {
 	SchellingOrgSimOptions
-} from './simulators/types/schelling-org.js';
+} from './simulators/types/schelling-org.GENERATED.js';
 
 import {
 	StandingOvationSimOptions

commit 673ef95e4156c1c981cded0d3018db17ab6a3f58

Fixed a bug introduced in bd396df7ff07d43f3aab52dbc1a7c907d770f1f2 with bad import.
Forgot to update the import for schelling-org to the generated type.

Part of #76.

src/simulators/schelling-org.ts
@@ -28,7 +28,7 @@ import {
 	CommunicationStrategy,
 	DisplayValue,
 	NorthStarOptions
-} from './types/schelling-org.js';
+} from './types/schelling-org.GENERATED.js';
 
 const SCHELLING_ORG_SIMULATION_NAME = 'schelling-org';
 