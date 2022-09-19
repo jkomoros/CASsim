ea2d1be7c19a24898c1c353c066e7fb9075ddeee
Alex Komoroske <jkomoros@gmail.com>
2022-09-19 10:28:47 -1000

    Move to a flow that generates the emoji constants automatically via `npm run generate:internal:emojis`
    Currently it's a semantic no-op but soon it will be useful for generating gendered emoji.
    
    You can't use `as const` type without it being an object literal, which means that automatically generating
    e.g. gendered and skin toned versions of people emoji would break the explicit enumerations of KnownEmoji*.
    
    Part of #68.

---
+++ tools/emojis.ts
@@ -0,0 +1,40 @@
+import * as fs from "fs";
+
+import data from './data/emojis.json' assert { type: "json" }
+
+const OUTPUT_FILE = 'src/constants-emoji.GENERATED.ts';
+
+const ROTATION_TYPES = [
+	'ROTATION_DOWN',
+	'ROTATION_UP',
+	'ROTATIION_LEFT',
+	'ROTATION_RIGHT'
+];
+
+const JSON_REPLACEMENTS = {
+	...Object.fromEntries(ROTATION_TYPES.map(typ => ['"' + typ + '"', typ]))
+};
+
+const generateEmojis = () => {
+	let output = JSON.stringify(data.emojis, null, '\t');
+	
+	for (const [find, replace] of Object.entries(JSON_REPLACEMENTS)) {
+		output = output.split(find).join(replace);
+	}
+
+	const rotationTypesToInclude = ROTATION_TYPES.filter(typ => output.includes(typ));
+
+	const fileContents = `//Generated via \`npm run generate:internal:emojis\`
+
+import {
+	${rotationTypesToInclude.join(',\n\t')}
+} from './constants.js';
+
+export const GENERATED_RAW_EMOJIS = ${output} as const;`;
+	fs.writeFileSync(OUTPUT_FILE, fileContents);
+};
+
+
+(async() => {
+	generateEmojis();
+})();
