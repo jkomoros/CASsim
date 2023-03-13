9ae08cbd5159721386b155f74981d47477c68894
Alex Komoroske <jkomoros@gmail.com>
2023-03-13 16:33:23 -0400

    Add stub of tests for coordinatesmap.
    Part of #87.

---
+++ test/coordinates_map/test.js
@@ -0,0 +1,42 @@
+/*eslint-env node*/
+
+import {
+	CoordinatesMap
+} from '../../src/coordinates-map.js';
+
+import assert from 'assert';
+
+const makeDefaultItems = () => {
+	return [
+		{
+			id: '0',
+			x: 100,
+			y: 100,
+			radius: 0,
+			otherProperty: '0',
+		},
+		{
+			id: '1',
+			x: 50,
+			y: 50,
+			//no radius, checking it's implicitly 0
+			otherProperty: '1'
+		},
+		{
+			id: '2',
+			x: 50,
+			y: 200,
+			radius: 25,
+			otherProperty: '2',
+		}
+	];
+};
+
+describe('CoordinatesMap', () => {
+	it('Basic all options', async () => {
+		const defaultItems = makeDefaultItems();
+		const map = new CoordinatesMap(null, defaultItems);
+		const result = map.getAllObjects();
+		assert.deepStrictEqual(result, defaultItems);
+	});
+});
